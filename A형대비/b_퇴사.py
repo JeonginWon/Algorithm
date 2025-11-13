00c6d731faa21ee1f54e7545627cf666bfd10f71
JeongIn <122220184+JeonginWon@users.noreply.github.com>
2025-11-13 22:04:16 +0900

    백준_퇴사_완성commit

--- A형대비/b_퇴사.py
+++ A형대비/b_퇴사.py
@@ -1,21 +1,24 @@
 # 퇴사 14051
-# 계산하는 함수
+
 
 N = int(input())
 time = []
 pay = []
-for n in range(N):
-    # Time, Pay
+
+for _ in range(N):
     t, p = map(int, input().split())
     time.append(t)
     pay.append(p)
 
-    #DP선언
-    dp = [0]*(N+1)
-    for i in range(1, N+1):
-        if time[i-1] <= N-i+1: #근무가능
-            dp[i] = max(dp[i-1], )
-        else:
-            pass
-            #근무 불가능
+    # DP배열
+dp = [0]*(N+1) # N+1 까지 생성 / 여기에 누적 수익금 저장
+
+for i in range(N-1, -1, -1): # bottom-up
+    # i번째 날 상담을 했는데? 퇴사일 넘으면
+    if i + time[i] > N: # 근무 불가
+        dp[i] = dp[i + 1] # 다음 날 이익 그대로 
+    else: # 근무 가능
+        # 근무 yes or no 선택을 했을 때 중 큰 값 선택
+        dp[i] = max(pay[i] + dp[i + time[i]], dp[i + 1])
             
+print(dp[0])
