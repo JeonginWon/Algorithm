e9d262cc0ea7d2e9302d1d8bf6218c4f24a0c3b0
jeonginWon <jeongin323@naver.com>
2025-11-13 17:51:37 +0900

    commit

--- A형대비/b_퇴사.py
+++ A형대비/b_퇴사.py
@@ -2,12 +2,21 @@
 # 계산하는 함수
 
 N = int(input())
-
+time = []
+pay = []
 for n in range(N):
-    # T, P
-   
+    # Time, Pay
+    t, p = map(int, input().split())
+    time.append(t)
+    pay.append(p)
 
     #DP선언
     dp = [0]*(N+1)
     for i in range(1, N+1):
-        dp[i] = max(dp[i-1])
+        if time[i-1] <= N-i+1: #근무가능
+            dp[i] = max(dp[i-1], )
+        else:
+            pass
+            #근무 불가능
+            
+# print(time)
